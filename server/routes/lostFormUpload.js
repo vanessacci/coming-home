4a981aa11e300fed121de216ddd4431863a9c6c7
harukaaa-u <harukaaa.u@gmail.com>
2024-02-03 10:22:31 -0800

    routes folder and lost form post method

--- server/routes/lostFormUpload.js
+++ server/routes/lostFormUpload.js
@@ -1,13 +1,15 @@
 let express = require('express');
 let router = express.Router();
-const lostSchema = require('../models/Lost');
+const {Lost} = require('../models');
 
 router.post('/api/lostFormUpload', async (req, res) => {
     try {
-      const lostData = new lostSchema(req.body);
-      const savedPet = await newPet.save();
-      res.status(200).json({ message: 'Pet data saved successfully', data: savedPet });
+      const lostData = new Lost(req.body);
+      const savedLostData = await lostData.save();
+      res.status(200).json({ message: 'Pet data saved successfully', data: savedLostData });
     } catch (error) {
       res.status(500).json({ message: 'Error saving pet data', error });
     }
   });
+
+  module.exports = router;
