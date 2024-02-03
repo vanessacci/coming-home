a2d273f641c0b20b12e194eb42bb0cfffaf8ff35
harukaaa-u <harukaaa.u@gmail.com>
2024-02-03 09:30:46 -0800

    connecting front and back, serve React app

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
+const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
 
 connectDB();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
