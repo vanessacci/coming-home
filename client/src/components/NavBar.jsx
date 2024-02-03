f83cc4cc9ce94973a11bfb713231098068e01126
harukaaa-u <harukaaa.u@gmail.com>
2024-02-03 10:21:40 -0800

    NavBar name and route change

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import LogOutButton from './LogoutButton';
 import { useNavigate } from 'react-router-dom';
 import '../stylesheets/navbar.css'; 
 
 const NavBar = () => {
   const navigate = useNavigate();
 
-  const toWelcome = () => {
-      navigate('/welcome');
+  const toHome = () => {
+      navigate('/');
   };
 
-  const toActivity = () => {
-    navigate('/activity');
+  const toLostPage = () => {
+    navigate('/lost-page');
   };
 
-  const toFriends = () => {
-      navigate('/friends');
+  const toFoundPage = () => {
+      navigate('/found-page');
   };
 
-  const toAccount = () => {
-    navigate('/account');
-  };
+
 
   return(
         <div className="nav-bar">
         <ul className="nav-list">
-            <button onClick={toWelcome}>Home</button>
-            <button onClick={toActivity}>Lost Page</button>
-            <button onClick={toFriends}>Found Page</button>
-            <button onClick={toAccount}>Account</button>
+            <button onClick={toHome}>Home</button>
+            <button onClick={toLostPage}>Lost Page</button>
+            <button onClick={toFoundPage}>Found Page</button>
         </ul>
         </div>
     );
