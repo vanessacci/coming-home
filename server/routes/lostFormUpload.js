98554ed226653d36bb75e18360b9258bb6f8c355
harukaaa-u <harukaaa.u@gmail.com>
2024-02-03 10:40:58 -0800

    update lost form route

--- server/routes/lostFormUpload.js
+++ server/routes/lostFormUpload.js
@@ -5,6 +5,7 @@ const {Lost} = require('../models');
 router.post('/api/lostFormUpload', async (req, res) => {
     try {
       const lostData = new Lost(req.body);
+      console.log(req.body);
       const savedLostData = await lostData.save();
       res.status(200).json({ message: 'Pet data saved successfully', data: savedLostData });
     } catch (error) {
