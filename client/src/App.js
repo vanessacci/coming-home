ca2c45ddbfbd4d5e4888b5228abf2181f39cf33d
harukaaa-u <harukaaa.u@gmail.com>
2024-02-03 09:28:53 -0800

    lost form route added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import FoundPostPage from "./components/NewFoundPostPage";
+import LostForm from "./components/LostForm";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/found-post" element={<FoundPostPage />} />
+        <Route path="/lost-form" element={<LostForm />} />
       </Routes>
     </Router>
   );
